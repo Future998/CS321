84025917bb2bd87487993efd1d34dfb5128378a5
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 13:33:38 +0300

    Update MPI1Proc4.cpp

--- MPI1Proc/MPI1Proc4.cpp
+++ MPI1Proc/MPI1Proc4.cpp
@@ -2,7 +2,7 @@
 #include "mpi.h"
 void Solve()
 {
-    Task("MPI1Proc3");
+    Task("MPI1Proc4");
     int flag;
     MPI_Initialized(&flag);
     if (flag == 0)
@@ -10,14 +10,9 @@ void Solve()
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
-        double n;
+    if (rank % 2 == 0) {
+        int n;
         pt >> n;
-        pt << -n;
-        Show("-X=", -n);
-    }   
-    else {
-        pt << rank;
-        Show("rank=", rank);
+        pt << 2 * n;
     }
 }
