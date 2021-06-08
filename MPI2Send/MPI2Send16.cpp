24f6d6c23f8c1d9ce07560805ad54c9008092be0
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 18:13:57 +0300

    Update MPI2Send16.cpp

--- MPI2Send/MPI2Send16.cpp
+++ MPI2Send/MPI2Send16.cpp
@@ -1,1 +1,27 @@
-
+#include "pt4.h"
+#include "mpi.h"
+void Solve()
+{
+    Task("MPI2Send16");
+    int flag;
+    MPI_Initialized(&flag);
+    if (flag == 0)
+        return;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int n;
+    double num;
+    pt >> n;
+    if (n) {
+        for (int i = 0; i < size; ++i) {
+            if (i == rank) continue;
+            pt >> num;
+            MPI_Send(&num, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        }
+    }
+    else {
+        MPI_Recv(&num, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        pt << num;
+    }
+}
