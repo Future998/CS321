238d0db3ae82fccaf935261dfc1db11f676be0d7
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 13:44:43 +0300

    Create MPI1Proc5.cpp

---
+++ MPI1Proc/MPI1Proc5.cpp
@@ -0,0 +1,23 @@
+#include "pt4.h"
+#include "mpi.h"
+void Solve()
+{
+    Task("MPI1Proc5");
+    int flag;
+    MPI_Initialized(&flag);
+    if (flag == 0)
+        return;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank % 2) {
+        double n;
+        pt >> n;
+        pt << 2 * n;
+    }
+    else {
+        int n;
+        pt >> n;
+        pt << 2 * n;
+    }
+}
