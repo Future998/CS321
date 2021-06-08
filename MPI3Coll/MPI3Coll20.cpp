5fda74ccc1abdf15e53ef3dff8939f5e69c5f640
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 19:44:40 +0300

    Update MPI3Coll20.cpp

--- MPI3Coll/MPI3Coll20.cpp
+++ MPI3Coll/MPI3Coll20.cpp
@@ -1,10 +1,8 @@
 #include "pt4.h"
 #include "mpi.h"
-#include <vector>
-
 void Solve()
 {
-	Task("MPI3Coll19");
+	Task("MPI3Coll20");
 	int flag;
 	MPI_Initialized(&flag);
 	if (flag == 0)
@@ -12,14 +10,12 @@ void Solve()
 	int rank, size;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int sz = size + 5;
-	std::vector<int> n(sz);
-	for (int i = 0; i < sz; ++i) 
-		pt >> n[i];
-	std::vector<int> rez(sz);
-	MPI_Reduce(&n[0], &rez[0], sz, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		for (auto& i : rez)
-			pt << i;
-	}
+	double* nums = new double[size + 5];
+	for (int i = 0; i < size + 5; ++i)
+		pt >> nums[i];
+	double* result = new double[size + 5];
+	MPI_Reduce(nums, result, size + 5, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+	if (rank == 0)
+		for (int i = 0; i < size + 5; ++i)
+			pt << result[i];
 }
