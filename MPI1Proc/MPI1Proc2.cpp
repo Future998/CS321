a9eb9de85f39b5d94ccd9f599ea4afeb3261842f
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 13:27:34 +0300

    Update MPI1Proc2.cpp

--- MPI1Proc/MPI1Proc2.cpp
+++ MPI1Proc/MPI1Proc2.cpp
@@ -1,1 +1,21 @@
-
+#include "pt4.h"
+#include "mpi.h"
+void Solve()
+{
+    Task("MPI1Proc2");
+    int flag;
+    MPI_Initialized(&flag);
+    if (flag == 0)
+        return;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int n;
+    pt >> n;
+    pt << 2 * n;
+    if (rank == 0) {
+        pt << size;
+        ShowLine(2 * n);
+        ShowLine(size);
+    }
+}
