125cd4acedb7a33ba403fd292f36d07211ffc3b3
Future998 <78866365+Future998@users.noreply.github.com>
2021-06-08 21:03:34 +0300

    Update MPI3Coll27.cpp

--- MPI3Coll/MPI3Coll27.cpp
+++ MPI3Coll/MPI3Coll27.cpp
@@ -1,27 +1,21 @@
 #include "pt4.h"
 #include "mpi.h"
-#include <vector>
-
 void Solve()
 {
-	Task("MPI3Coll26");
-	int flag;
-	MPI_Initialized(&flag);
-	if (flag == 0)
-		return;
-	int rank, size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int sz = size * (size + 3) / 2;
-	std::vector<int> n(sz);
-	int* rcv = new int[size];
-	for (int i = 0, j = 2; i < size; i++, j++)
-		rcv[i] = j;
+    Task("MPI3Coll27");
+    int flag;
+    MPI_Initialized(&flag);
+    if (flag == 0)
+        return;
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	int sz = size + 5;
+	std::vector<double> n(sz);
 	for (int i = 0; i < sz; ++i) 
 		pt >> n[i];
-	int sz_re = rank + 2;
-	std::vector<int> re(sz_re);
-	MPI_Reduce_scatter(&n[0], &re[0], rcv, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+	std::vector<double> re(sz);
+	MPI_Scan(&n[0], &re[0], sz, MPI_DOUBLE, MPI_PROD, MPI_COMM_WORLD);
 	for (auto& i : re) 
 		pt << i;
 }
